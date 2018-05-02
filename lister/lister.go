20689a7169d7b8cddbac4b16e9d05881b7e2364a
Scot W. Stevenson <scot.stevenson@gmail.com>
2018-05-03 00:43:20 +0200

    Added skeleton for lister

---
+++ lister/lister.go
@@ -0,0 +1,15 @@
+// Lister Package for the GoAsm65816 assembler
+// Scot W. Stevenson <scot.stevenson@gmail.com>
+// First version: 02. May 2018
+// This version: 02. May 2018
+
+package lister
+
+import (
+	"fmt"
+)
+
+func Lister() {
+	fmt.Println("(At lister)")
+
+}
