7db9a724c2519e74461b4a510366edaba9f6825a
Scot W. Stevenson <scot.stevenson@gmail.com>
2018-05-11 01:44:17 +0200

    Better formatter output

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -1,10 +1,12 @@
 // Formatter Package for the Cthulhu Assembler
 // Scot W. Stevenson <scot.stevenson@gmail.com>
 // First version: 04. May 2018
-// This version: 10. May 2018
+// This version: 11. May 2018
 
 // The formatter produces a cleanly formatted source file, following
 // the example of gofmt
+
+// TODO remove double empty lines
 
 package formatter
 
@@ -24,34 +26,45 @@ func Formatter(tl *[]token.Token) {
 	for _, t := range *(tl) {
 
 		switch t.Type {
-		case token.EOL:
+
+		case token.EOL, token.EMPTY, token.EOF:
 			fmt.Print("\n")
 			continue
 
-		case token.LOCAL_LABEL:
+		case token.LOCAL_LABEL, token.LABEL, token.ANON_LABEL:
 			fmt.Print(t.Text, "\n")
 			continue
 
-		case token.LABEL:
-			fmt.Print(t.Text, "\n")
+		case token.STRING:
+			fmt.Printf("\"%s\"", t.Text)
+			continue
+
+		case token.BIN_NUM:
+			fmt.Print("%", t.Text)
+			continue
+
+		case token.HEX_NUM:
+			fmt.Print("$", t.Text)
 			continue
 
 		case token.COMMENT:
 			fmt.Print(t.Text)
 			continue
 
-		case token.DIREC:
+		case token.DIREC, token.DIREC_PARA:
 			fmt.Print(indent1)
 			fmt.Print(t.Text, " ")
 			continue
 
-		case token.SAN_0:
+		case token.SAN_0, token.SAN_1, token.SAN_2:
 			fmt.Print(indent2)
 			fmt.Print(t.Text, " ")
 			continue
+
+		default:
+			fmt.Print(t.Text, " ")
 		}
 
-		t.Print()
 	}
 
 	fmt.Print("\n")
