389c6746985886319ab30c0792ca64e428c4ae7d
Scot W. Stevenson <scot.stevenson@gmail.com>
2018-05-10 22:04:30 +0200

    Added skeleton for RPN (math) routines

---
+++ rpn/rpn.go
@@ -0,0 +1,7 @@
+// Reverse Polish Notation (RPN) math processing
+// for the Cthulhu Assembler
+// Scot W. Stevenson <scot.stevenson@gmail.com>
+// First version: 10. May 2018
+// This version: 10. May 2018
+
+package rpn
